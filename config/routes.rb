56f3a51e50626ad53ceb8933a00b9efb957c2a90
Junior <junior@l3arn.net>
2012-05-06 18:32:54 -0400

    Make a base USer Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+
+  get "users/new"
+
+  root to: 'static_pages#home'
+
+  get '/signup',  to: 'users#new'
+  get '/help',    to: 'static_pages#help'
+  get '/about',   to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
